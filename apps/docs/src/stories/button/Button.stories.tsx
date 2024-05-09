6890016010b6374c984259a62e82f0fd30422e5a
Robby Pambudi <robby.pambudi10@gmail.com>
2024-05-09 15:49:42 +0700

    feat: delete unusable stories

---
+++ apps/docs/src/stories/button/Button.stories.tsx
@@ -0,0 +1,129 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button, ButtonStyle } from '@flush/ui/button';
+
+type ButtonVariants = keyof typeof ButtonStyle.variants;
+type ButtonSizes = keyof typeof ButtonStyle.sizes;
+
+const variants = Object.keys(ButtonStyle.variants) as ButtonVariants[];
+const sizes = Object.keys(ButtonStyle.sizes) as ButtonSizes[];
+
+// eslint-disable-next-line storybook/story-exports
+const meta = {
+  title: 'Atoms/Button',
+  component: Button,
+  argTypes: {
+    variant: {
+      description: 'Defines the variant of the button',
+      table: {
+        type: { summary: variants.join('|') },
+        defaultValue: { summary: 'default' },
+      },
+      control: 'select',
+      options: variants,
+    },
+    size: {
+      description: 'Defines the size of the button',
+      table: {
+        type: { summary: sizes.join('|') },
+        defaultValue: { summary: 'md' },
+      },
+      control: 'select',
+      options: sizes,
+    },
+    children: {
+      description:
+        'Children components, usually text or an icon, that will be rendered inside the button',
+      // table: {
+      //   defaultValue: {  },
+      // },
+    },
+    className: {
+      description: 'Override or extend the styles applied to the component',
+      control: 'text',
+      table: {
+        category: 'Override/extend',
+        type: { summary: 'string' },
+        defaultValue: { summary: '' },
+      },
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    size: 'md',
+    children: 'My button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    ...Primary.args,
+    variant: 'secondary',
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    ...Primary.args,
+    variant: 'outline',
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    ...Primary.args,
+    variant: 'ghost',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    ...Primary.args,
+    size: 'lg',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    ...Primary.args,
+    size: 'md',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    ...Primary.args,
+    size: 'sm',
+  },
+};
+
+export const All: Story = {
+  render: () => {
+    return (
+      <>
+        {variants.map((variant) => (
+          <div className='flex items-baseline space-x-2' key={variant}>
+            <div className='w-[100px] text-sm text-slate-500'>{variant}</div>
+            <div className='mb-4 flex items-center space-x-2'>
+              {sizes.map((size) => (
+                <Button
+                  variant={variant}
+                  size={size}
+                  key={`${variant}-${size}`}
+                >
+                  {variant} {size}
+                </Button>
+              ))}
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  },
+};
--- packages/ui/src/components/button/button.tsx
+++ packages/ui/src/components/button/button.tsx
@@ -4,70 +4,18 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { forwardRef } from 'react';
 import { LucideIcon, RefreshCwIcon } from 'lucide-react';
 import { cn } from '../../types/helper';
+import { ButtonStyle } from './style';
 
-const buttonVariants = cva(
-  [
-    'inline-flex items-center justify-center rounded-md',
-    'font-medium ring-offset-background transition-colors',
-    'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring',
-    'focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
-  ],
-  {
-    variants: {
-      variant: {
-        primary: [
-          'bg-primary-500 text-white hover:bg-primary-600',
-          'border border-primary-600',
-          'active:bg-primary-700 active:border-primary-700',
-          'focus-visible:ring-primary-400 focus-visible:border-primary-700',
-          'disabled:bg-primary-400 disabled:border-primary-400',
-        ],
-        secondary: [
-          'bg-secondary-600 text-white hover:bg-secondary-700',
-          'border border-secondary-700',
-          'active:bg-secondary-800 active:border-secondary-800',
-          'focus-visible:ring-secondary-400 focus-visible:border-secondary-800',
-          'disabled:bg-secondary-400 disabled:border-secondary-400',
-        ],
-        warning: [
-          'bg-yellow-600 text-white hover:bg-yellow-700',
-          'border border-yellow-700',
-          'active:bg-yellow-800 active:border-yellow-800',
-          'focus-visible:ring-yellow-400 focus-visible:border-yellow-800',
-          'disabled:bg-yellow-400 disabled:border-yelllow-400',
-        ],
-        danger: [
-          'bg-red-500 text-white hover:bg-red-600',
-          'border border-red-600',
-          'active:bg-red-700 active:border-red-700',
-          'focus-visible:ring-red-400 focus-visible:border-red-700',
-          'disabled:bg-red-400 disabled:border-red-400',
-        ],
-        outline: [
-          'text-typo',
-          'border border-gray-300',
-          'active:bg-typo-divider disabled:bg-typo-divider',
-          'hover:bg-light focus-visible:ring-primary-400',
-        ],
-        ghost: [
-          'text-primary-500',
-          'shadow-none',
-          'hover:bg-primary-50 focus-visible:ring-primary-400',
-          'active:bg-primary-100 disabled:bg-primary-100',
-        ],
-      },
-      size: {
-        base: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3 text-sm',
-        lg: 'h-11 rounded-md px-8',
-      },
-    },
-    defaultVariants: {
-      variant: 'primary',
-      size: 'base',
-    },
+const buttonVariants = cva(ButtonStyle.base, {
+  variants: {
+    variant: ButtonStyle.variants,
+    size: ButtonStyle.sizes,
   },
-);
+  defaultVariants: {
+    variant: 'primary',
+    size: 'base',
+  },
+});
 
 export type ButtonProps = {
   isLoading?: boolean;
@@ -172,4 +120,4 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   },
 );
 
-export default Button;
+export { Button, buttonVariants, ButtonStyle };
